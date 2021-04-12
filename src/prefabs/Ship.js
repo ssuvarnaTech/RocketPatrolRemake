e7a6e7ee8cc32f6ddae4ff5ceca58235bdf0cd91
ssuvarnaTech <ssuvarna@ucsc.edu>
2021-04-11 22:46:47 -0700

    tuned the movement speed

--- src/prefabs/Ship.js
+++ src/prefabs/Ship.js
@@ -3,4 +3,10 @@ class Ship extends Phaser.GameObjects.Sprite{
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
     }
+    update(){
+        this.x -= 2;
+        if(this.x < -this.width){
+            this.x = game.config.width;
+        }
+    }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,7 +11,9 @@ class Play extends Phaser.Scene{
     create(){
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setOrigin(0.5, 0);
-        this.ship1 = new Ship(this,100,200,'spaceship');
+        this.ship1 = new Ship(this, 100, 200, 'spaceship').setOrigin(0.5, 1);
+        this.ship2 = new Ship(this, 300, 240, 'spaceship').setOrigin(0.5, 1);
+        this.ship3 = new Ship(this, 380, 300, 'spaceship').setOrigin(0.5, 1);
         //green UI background rectangle
         this.add.rectangle(0, borderUISize + borderPadding,
              game.config.width, borderUISize * 2, 
@@ -35,6 +37,9 @@ class Play extends Phaser.Scene{
     update(){
         this.starfield.x -=4;
         this.p1Rocket.update();
+        this.ship1.update();
+        this.ship2.update();
+        this.ship3.update();
 
     }
 }
